e343351adbfed1fc700fade4db2cb8d8c852506e
suvithamohanapriya <swarnaprabhamaha@gmail.com>
2019-01-31 16:27:54 +0000

    Create StringPrint.java

---
+++ StringPrint.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+public class StringPrint
+{
+  public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+    int k = sc.nextInt();
+    for(int i = 0;i<k;i++)
+    System.out.println(str);
+  }
+}
