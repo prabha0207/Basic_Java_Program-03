f42a1b89c87f250bbec1d1da36d8c2b2834d2b9b
suvithamohanapriya <swarnaprabhamaha@gmail.com>
2019-02-01 12:25:47 +0530

    Create CompareTwoString.java

---
+++ CompareTwoString.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.io.*;
+public class CompareTwoString 
+{
+		public static void main(String[] args) 
+    {
+			Scanner in=new Scanner(System.in);
+			String s1=in.next();
+			String s2=in.next();
+			if(s1.length()>s2.length())
+			{
+				System.out.print(s1);
+			}
+      else if(s1.length()<s2.length())
+			{
+				System.out.println(s2);
+			}
+      else
+			{
+				System.out.print(s1);
+			}
+		}
+	}
