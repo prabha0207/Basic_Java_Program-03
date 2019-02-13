a70600296fc32703c55abe288c021c5cffd27b02
suvithamohanapriya <swarnaprabhamaha@gmail.com>
2019-02-13 14:07:08 +0530

    Create NumericDigit.java

---
+++ NumericDigit.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.
+public class NumericDigit
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		long n=s.nextLong();
+		String str=String.valueOf(n);
+		System.out.println(str.length());
+		s.close();
+	}
+}
