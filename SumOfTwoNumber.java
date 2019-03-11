443f51ac593e9ae5e1ecafe2dedbd41dcd3a4f44
suvithamohanapriya <swarnaprabhamaha@gmail.com>
2019-03-11 15:51:52 +0530

    Create SumOfTwoNumber.java

---
+++ SumOfTwoNumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class SumOfTwoNumber
+{
+    public static void main(String[] args) 
+    {
+        int n1,n2;
+        Scanner s = new Scanner(System.in); 
+        n1 = s.nextInt();
+        n2 = s.nextInt();
+      
+        System.out.print(n1+n2);
+    }
+}
